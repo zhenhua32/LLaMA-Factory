513f710db75a97ce3a0bfd1e1bf29f368006d674
zhenhua32 <zhenhua32@gmail.com>
2025-05-18 09:04:51 +0800

    save

--- my_notebook/init_npu.sh
+++ my_notebook/init_npu.sh
@@ -2,9 +2,10 @@
 pip install modelscope
 # /home/service/.local/bin/modelscope
 export PATH=$PATH:/home/service/.local/bin
-modelscope download --model Qwen/Qwen3-32B  --local_dir /cache/Qwen3-32B
+modelscope download --model Qwen/Qwen3-8B  --local_dir /cache/Qwen3-8B
 
 # 2. 下载 Ascend CANN 依赖
+pip install openi
 openi model download tobe/qwen3_train npu --save_path /cache/npu
 cd /cache/npu
 chmod +x ./Ascend-cann-toolkit_8.1.RC1_linux-aarch64.run
@@ -17,7 +18,8 @@ chmod +x ./Ascend-cann-kernels-910b_8.1.RC1_linux-aarch64.run
 
 # 3. 安装 llamafactory
 pip install -e ".[torch-npu,metrics]"
+pip install -U pillow
 
 export ASCEND_SLOG_PRINT_TO_STDOUT=1 #日志打屏, 可选
 export ASCEND_GLOBAL_LOG_LEVEL=1 #日志级别常用 1 INFO级别; 3 ERROR级别
-llamafactory-cli train examples/train_lora/qwen3_lora_sft_npu.yaml
+llamafactory-cli train examples/train_lora/qwen3_lora_sft_npu.yaml > run.log
